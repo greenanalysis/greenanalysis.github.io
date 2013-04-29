4468179f57cebd1a131232a55573666729971489
Thomas McGoey-Smith <thomasmcgoeysmith@gmail.com>
2013-04-29 17:15:50 -0600

    css

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'compass-h5bp'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = ""
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
